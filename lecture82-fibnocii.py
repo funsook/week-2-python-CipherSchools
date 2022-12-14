ab8f5a656fed572f7be6e93e83a54ed65ddb70f7
funsook <119058491+funsook@users.noreply.github.com>
2022-12-14 05:25:34 -0800

    Create lecture82-fibnocii.py

---
+++ lecture82-fibnocii.py
@@ -0,0 +1,17 @@
+#fibonacii series
+def fib(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    if n==2:
+        print(a,b)
+    else:
+        print(a,b,end=" ")
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+            print(b,end=" ")
+
+fib(20)
