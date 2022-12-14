00699cfb379df0e43c05a3308bc753909012221e
funsook <119058491+funsook@users.noreply.github.com>
2022-12-14 05:31:17 -0800

    Create lecture91-is vs equal.py

---
+++ lecture91-is vs equal.py
@@ -0,0 +1,5 @@
+fruits =['orange','apple','pear','banana','kiwi']
+fruits2 =['orange','apple','pear','banana','kiwi']
+fruits1 =['orange','apple','pear','banana','kiwi','Guava',]
+print(fruits==fruits2)
+print(fruits is fruits1)
