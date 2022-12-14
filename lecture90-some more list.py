025335ea9e70169cda7bfd45c52bebbaf4212d1a
funsook <119058491+funsook@users.noreply.github.com>
2022-12-14 05:30:35 -0800

    Create lecture90-some more list.py

---
+++ lecture90-some more list.py
@@ -0,0 +1,11 @@
+fruits =['orange','apple','pear','banana','kiwi','guava','orange']
+print(fruits.count('orange'))
+
+num=[122,2,3,4,50,34,345,33,5]
+num.sort()
+print(num)
+num.sort(reverse=True)
+print(num)
+
+num.clear()
+print(num)
